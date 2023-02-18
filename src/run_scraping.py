2d6ab9d4952de46ac97b5706e4f9eebe8b01cdf1
Siergej Sobolewski <108773983+SSobol77@users.noreply.github.com>
2023-02-18 18:00:31 +0100

    update db

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,56 +1,125 @@
-import codecs
+import asyncio
 import os, sys
-
+import codecs
+import django
+import datetime as dt
 
 # устанавливаеи абсолютный путь для DJANGO к проекту, т.к. на машине разработчика и на сервере
 # где будет заинсталировано разные пути и папки :
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
 os.environ["DJANGO_SETTINGS_MODULE"] = "ScrapingService.settings"
-import django
+
 django.setup()  # запуск Django !!!!!!!!!!!!!!!
-from sqlite3 import DatabaseError # переносим импорт сюда, т.е. уже после запуска Django
-
-
+from sqlite3 import DatabaseError  # переносим импорт сюда, т.е. уже после запуска Django
+from django.db import DatabaseError
 from scraping.parsers import *
+from scraping.models import Vacancies, Error, Url
+from django.contrib.auth import get_user_model
 
-from scraping.models import Vacancies, City, Language, Error
-
+# возвращаем юзера по умолчанию
+User = get_user_model()
 
 # прописываем парсерс фуикций и адресов:
 parsers = (
-    (work, 'https://www.work.ua/jobs-kyiv-python/'),
-    (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'),
-    (djinni, 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv'),
-    (rabota, 'https://rabota.ua/ua/zapros/python/%D0%BA%D0%B8%D0%B5%D0%B2')
+    (work, 'work'),
+    (dou, 'dou'),
+    (djinni, 'djinni'),
+    (rabota, 'rabota')
 )
 
-city = City.objects.filter(slug='kiev').first()  # приводим из кварисета к истенсу
-language = Language.objects.filter(slug='python').first()   # приводим из кварисета к истенсу
-
 jobs, errors = [], []
-for func, url in parsers:
-    j, e = func(url)
-    jobs += j
-    errors += e
+
+
+# функция которая возвращает уникальные наборы города и языка прогр. из Юзера зарегистрированного в системе:
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()  # values -позволяет получить список словарей, т.е. вместо города и языка получаем их id
+    # делаем settings_lst как set потому что может быть несколько пользователей с одинаковыми настройками делаем в виде генератора:
+    settings_lst = set((q['city_id'], q['language_id']) for q in qs)
+    return settings_lst  # здесь в settings_lst - храняться настройки по умолчанию для нашего набора значений
+
+
+# Функция для получения набора урлов (списка словарей), после выбора Юзером набора в виде списка  пары city_id и language_id
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+    for pair in _settings:
+        if pair in url_dict:
+            tmp = {}
+            tmp['city'] = pair[0]
+            tmp['language'] = pair[1]
+            url_data = url_dict.get(pair)
+            if url_data:
+                tmp['url_data'] = url_dict.get(pair)
+                urls.append(tmp)
+    return urls
+
+
+async def main(value):
+    func, url, city, language = value
+    job, err = await loop.run_in_executor(None, func, url, city, language)
+    errors.extend(err)
+    jobs.extend(job)
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+
+# асинхронный запуск функции скрапинга:
+async def main(value):
+    func, url, city, language = value
+    job, err = await loop.run_in_executor(None, func, url, city, language)
+    errors.extend(err)
+    jobs.extend(job)
+
+settings = get_settings()
+url_list = get_urls(settings)
+
+loop = asyncio.get_event_loop()
+tmp_tasks = [(func, data['url_data'][key], data['city'], data['language'])
+             for data in url_list
+             for func, key in parsers]
+
+# for data in url_list:
+#
+#     for func, key in parsers:
+#         url = data['url_data'][key]
+#         j, e = func(url, city=data['city'], language=data['language'])
+#         jobs += j
+#         errors += e
+
+if tmp_tasks:
+    tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
+    loop.run_until_complete(tasks)
+    loop.close()
+
 
 for job in jobs:
 
-            # поскольку сделано таким образом что ключи в функциях формируются таким же самым образом как
-            # и названные имена полей title, url, description и company, то можно здесь раскрыть данный словарь job,
-            # причем city и language должны быть указаны явно :
-    v = Vacancies(**job, city=city, language=language )
-            # для модели вакансий мы установили уникальный url, т.е. существует риск захвата с сайта старых вакансий,
-            # если он медленно обнавляется или в выходные дни, т.е. собрать уже имеющиеся в нашей базе вакансии, чтобы их
-            # не дублировать, самый эффективный способ это помещение в трай :
+    # поскольку сделано таким образом что ключи в функциях формируются таким же самым образом как
+    # и названные имена полей title, url, description и company, то можно здесь раскрыть данный словарь job,
+    # причем city и language будут автоматически прописываться соответственно с тем что ест в моделе вакансии :
+    v = Vacancies(**job)
+    # для модели вакансий мы установили уникальный url, т.е. существует риск захвата с сайта старых вакансий,
+    # если он медленно обнавляется или в выходные дни, т.е. собрать уже имеющиеся в нашей базе вакансии, чтобы их
+    # не дублировать, самый эффективный способ это помещение в трай :
     try:
         v.save()  # вакансия сохранится
     except DatabaseError:
         pass
 # если появляются ошибки они будут добавлены в список errors и при его наличии запишем его в базуданных :
 if errors:
-    er = Error(data=errors).save()
+    qs = Error.objects.filter(timestamp=dt.date.today())
+    if qs.exists():
+        err = qs.first()
+        err.data.update({'errors': errors})
+        err.save()
+    else:
+        er = Error(data=f'errors:{errors}').save()
 
-#h = codecs.open('work.txt', 'w', 'utf-8')
-#h.write(str(jobs))
-#h.close()
+# h = codecs.open('work.txt', 'w', 'utf-8')
+# h.write(str(jobs))
+# h.close()
+ten_days_ago = dt.date.today() - dt.timedelta(10)
+Vacancies.objects.filter(timestamp__lte=ten_days_ago).delete()
---
+++ src/send_email.py
@@ -0,0 +1,119 @@
+import os, sys
+import django
+import datetime
+from django.core.mail import EmailMultiAlternatives
+
+from django.contrib.auth import get_user_model
+
+#  Устанавливаем абсолютный путь для DJANGO к проекту, т.к. на машине разработчика и на сервере
+#  где будет заинсталлировано разные пути и папки:
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ["DJANGO_SETTINGS_MODULE"] = "ScrapingService.settings"
+
+django.setup()  # запуск Django !!!!!!!!!!!!!!!
+from scraping.models import Vacancies, Error, Url
+from ScrapingService.settings import (
+    EMAIL_HOST_USER,
+    EMAIL_HOST, EMAIL_HOST_PASSWORD
+)
+
+ADMIN_USER = EMAIL_HOST_USER
+
+today = datetime.date.today()
+subject = f"Рассылка вакансий за {today}"  # заголовок письма
+text_content = f"Рассылка вакансий {today}"  # текстовое значение явно не используется
+from_email = EMAIL_HOST_USER   # указываем эмайл с которого будет непосредственно происходить отправка
+empty = '<h4> Сегодня обновления отсутствуют.</h4>'
+
+
+User = get_user_model()
+qs = User.objects.filter(send_email=True).values('city', 'language', 'email')
+users_dct = {}
+for i in qs:
+    users_dct.setdefault((i['city'], i['language']), [])
+    users_dct[(i['city'], i['language'])].append(i['email'])
+if users_dct:
+    params = {'city_id__in': [], 'language_id__in': []} # __in говорит о том что найти все значения принадлежащие этой паре
+    for pair in users_dct.keys():
+        params['city_id__in'].append(pair[0])
+        params['language_id__in'].append(pair[1])
+    qs = Vacancies.objects.filter(**params, timestamp=today).values()
+    vacancies = {}
+    for i in qs:
+        vacancies.setdefault((i['city_id'], i['language_id']), [])
+        vacancies[(i['city_id'], i['language_id'])].append(i)
+    for keys, emails in users_dct.items():
+        rows = vacancies.get(keys, [])
+        html = ''  # шаблончик
+        for row in rows:   # сформируем текст эмайла
+            html += f'<h3"><a href="{ row["url"] }">{ row["title"] }</a></h3>'
+            html += f'<p>{row["description"]} </p>'
+            html += f'<p>{row["company"]} </p><br><hr>'
+        _html = html if html else empty
+        for email in emails:   # отправляем эмайл:
+            to = email
+            msg = EmailMultiAlternatives(
+                subject, text_content, from_email, [to]
+            )
+            msg.attach_alternative(_html, "text/html")
+            msg.send()
+
+# отбор ошибок:
+qs = Error.objects.filter(timestamp=today)
+subject = ''
+text_content = ''
+to = ADMIN_USER
+_html = ''
+if qs.exists():
+    error = qs.first()
+    data = error.data.get('errors', [])
+    for i in data:
+        _html += f'<p"><a href="{ i["url"] }">Error: { i["title"] }</a></p><br>'
+    subject += f"Ошибки скрапинга {today}"
+    text_content += "Ошибки скрапинга"
+    data = error.data.get('user_data')
+    if data:
+        _html += '<hr>'
+        _html += '<h2>Пожелания пользователей </h2>'
+        for i in data:
+            _html += f'<p">Город: {i["city"]}, Специальность:{i["language"]},  Имейл:{i["email"]}</p><br>'
+        subject += f" Пожелания пользователей {today}"
+        text_content += "Пожелания пользователей"
+
+qs = Url.objects.all().values('city', 'language')
+urls_dct = {(i['city'], i['language']): True for i in qs}
+urls_err = ''
+for keys in users_dct.keys():  # есть ли ключ из словаря с юзерами в словаре с урлами
+    if keys not in urls_dct:
+        if keys[0] and keys[1]:
+            urls_err += f'<p"> Для города: {keys[0]} и специальности: {keys[1]} отсутствуют urls</p><br>'
+if urls_err:
+    subject += ' Отсутствующие urls '
+    _html += '<hr>'
+    _html += '<h2>Отсутствующие urls </h2>'
+    _html += urls_err
+
+if subject:
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(_html, "text/html")
+    msg.send()
+
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+#
+# msg = MIMEMultipart('alternative')
+# msg['Subject'] = 'Список вакансий за  {}'.format(today)
+# msg['From'] = EMAIL_HOST_USER
+# mail = smtplib.SMTP()
+# mail.connect(EMAIL_HOST, 25)
+# mail.ehlo()
+# mail.starttls()
+# mail.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+#
+# html_m = "<h1>Hello world</h1>"
+# part = MIMEText(html_m, 'html')
+# msg.attach(part)
+# mail.sendmail(EMAIL_HOST_USER, [to], msg.as_string())
+# mail.quit()
